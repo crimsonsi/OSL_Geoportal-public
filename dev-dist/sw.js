9c763cafddff95da3a083a09ace2591419ab48ad
mutetid <mutetidanz@gmail.com>
2025-09-24 11:29:23 +0300

    remove config-overrides.js and update dependencies including sass to version 1.93.2 and rollup to version 4.52.2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5aimf0ir4og"
+    "revision": "0.5ldp5i56378"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
